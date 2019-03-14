fa4ee83ec939450341fbda7df0e0ad7457fe9896
Dione Wilson <dionew1@gmail.com>
2019-03-14 17:15:26 -0600

    Update CORS Update packages Fix typo in migrations

--- server.js
+++ server.js
@@ -16,8 +16,14 @@ app.use(express.static(path.join(__dirname + '/public')));
 app.use(cors());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
+
+  if ('OPTIONS' == req.method) {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
 });
 
 app.locals.title = 'Hedgehog Party';
